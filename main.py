1ffce331908429f23c838b506652701420c0ff2b
YOUR_NAME <YOUR_EMAIL>
2022-10-11 12:43:25 +0300

    added .gitignore file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"status": "Working"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
