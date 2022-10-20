68124b2d51f24d654d233a2546cc45cdbfec2439
YOUR_NAME <YOUR_EMAIL>
2022-10-20 11:26:46 +0300

    BE-5

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from app import crud, models, schemas
 from app.database import SessionLocal
 from app.routes import router
 
+
 db = databases.Database(settings.DATABASE_URL)
 
 app = FastAPI()
@@ -48,46 +49,46 @@ async def root():
     return {"status": "Working"}
 
 
-def get_db():
+async def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.SignUpUser, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+    db_user = crud.Cruds.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return crud.Cruds.create_user(db=db, user=user)
 
 @app.post("/update-user/")
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user.id)
+    db_user = crud.Cruds.get_user(db, user_id=user.id)
     if db_user:
-        return crud.update_user(db=db,user=user)
+        return crud.Cruds.update_user(db=db,user=user)
     else:
         return HTTPException(status_code=400, detail="No such user")
 
 
 @app.get("/users/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+    users = crud.Cruds.get_users(db, skip=skip, limit=limit)
     return users
 
 @app.delete("/users/")
 def delete_user(user: schemas.UserDelete, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+    db_user = crud.Cruds.get_user_by_email(db, email=user.email)
     if db_user:
-        crud.delete_user(db=db, user=user)
+        crud.Cruds.delete_user(db=db, user=user)
         return HTTPException(status_code=200, detail="User deleted successfully")
     else:
         return HTTPException(status_code=400, detail="No such user")
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.Cruds.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
